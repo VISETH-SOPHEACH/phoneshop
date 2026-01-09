e23ce582482812d3f6b93dcd537b80dcc888ba90
viseth-max <visethsopheach@gmail.com>
2026-01-09 13:58:42 +0700

    Initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <header class="flex px-10 p-3 text-lg md:text-2xl bg-blue-600">
+      <h1 class="text-white">Viseth Phone Shop</h1>
+      <div class="flex gap-10 ml-auto mr-10">
+        <p>iOS</p>
+        <p>Android</p>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+  
+</script>
+
+<style lang="scss" scoped></style>
